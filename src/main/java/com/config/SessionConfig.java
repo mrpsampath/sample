0093d741bcf566b6813e7254b3557307ddabbbdd
mrpsampath <mr.psampath@gmail.com>
2018-03-26 21:45:14 -0400

    Modified Through eclipse

--- src/main/java/com/config/SessionConfig.java
+++ src/main/java/com/config/SessionConfig.java
@@ -17,7 +17,8 @@ public class SessionConfig {
 	@Bean
 	public SessionFactory getSessionFactory() {
 	    if (entityManagerFactory.unwrap(SessionFactory.class) == null) {
-	        throw new NullPointerException("factory is not a hibernate factory");
+
+	    	throw new NullPointerException("factory is not a hibernate factory");
 	    }
 	    
 	    return entityManagerFactory.unwrap(SessionFactory.class);
