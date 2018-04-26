e062564e59029dfb2348325978690162183a4aad
mrpsampath <mr.psampath@gmail.com>
2018-04-25 21:06:13 -0400

    Commited Employee Model

---
+++ src/main/java/com/models/EmployeeModel.java
@@ -0,0 +1,20 @@
+package com.models;
+
+public class EmployeeModel {
+
+	String name;
+	String lastName;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	
+}
